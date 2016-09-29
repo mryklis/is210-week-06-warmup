30be740d796fbfecb73a456d84158fee50202aeb
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-09-29 01:32:56 +0000

    work in progress

--- task_05.py
+++ task_05.py
@@ -2,5 +2,18 @@
 # -*- coding: utf-8 -*-
 """this module inverts the sequence of a list"""
 
-def flip_keys(to_flip):
-    
+
+to_flip = [(1, 2, 3), 'hello']
+
+##def flip_keys(to_flip =[(1, 2, 3), 'hello']):
+obj1 = to_flip[0]
+jbo1 = obj1[::-1]
+obj2 = to_flip[1]
+jbo2 = obj2[::-1]
+NEW = (jbo1, jbo2)
+print jbo1
+print jbo2
+print NEW
+##counter = 0
+##for obj in to_flip
+##
