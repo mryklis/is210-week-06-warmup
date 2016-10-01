4fd83c95ea01cea6c1c432f3615633c5d4e12af5
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-10-01 15:41:38 +0000

    task_05 competed

--- task_05.py
+++ task_05.py
@@ -3,17 +3,25 @@
 """this module inverts the sequence of a list"""
 
 
-to_flip = [(1, 2, 3), 'hello']
+def flip_keys(to_flip):
+    """reverses the order of objects in the inner list
 
-##def flip_keys(to_flip =[(1, 2, 3), 'hello']):
-obj1 = to_flip[0]
-jbo1 = obj1[::-1]
-obj2 = to_flip[1]
-jbo2 = obj2[::-1]
-NEW = (jbo1, jbo2)
-print jbo1
-print jbo2
-print NEW
-##counter = 0
-##for obj in to_flip
-##
+    Args:
+        to_flip (list): a list with an inner list.
+
+    Returns:
+        to_flip (list): with inner objects in reverse order.
+
+    Example:
+        >>>LIST = [(1, 2, 3), 'abc']
+        >>>NEW = flip_keys(LIST)
+        >>>print LIST
+        [(3, 2, 1), 'cba']
+    """
+
+    counter = 0
+    for obj in to_flip:
+        obj = to_flip[counter][::-1]
+        to_flip[counter] = obj
+        counter += 1
+    return to_flip
